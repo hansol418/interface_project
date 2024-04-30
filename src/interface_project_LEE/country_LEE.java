cd315319c07dd072a0fe58267000075dacf7f111
LeeHanSol <hslee4954@gmail.com>
2024-04-30 17:39:48 +0900

    0430

--- src/interface_project_LEE/country_LEE.java
+++ src/interface_project_LEE/country_LEE.java
@@ -2,5 +2,5 @@ package interface_project_LEE;
 
 public interface country_LEE {
 	public final static String present = "Royal Salute";
-
+	public final static String money = "euro";
 }
--- src/interface_project_LEE/sum_LEE.java
+++ src/interface_project_LEE/sum_LEE.java
@@ -1,5 +1,33 @@
 package interface_project_LEE;
 
-public class sum_LEE {
+public class sum_LEE extends memoryOfTrip{
 
+	@Override
+	public void place(String contryplace) {
+		System.out.println("슬로베니아 블레드 캐슬");
+		
+	}
+
+	@Override
+	public void food(String contryfood) {
+		System.out.println("소시지 요리");
+		
+	}
+
+	@Override
+	public void price(String contryprice) {
+		System.out.println("1000만원");
+		
+	}
+
+	@Override
+	public void time(String contrytime) {
+		System.out.println("13시간");
+		
+	}
+	
+	
+	
 }
+
+
